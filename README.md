77f7fc50d689c58ebbade5af2e783e757f67de62
yujinpan <yujinpan@cmstop.com>
2018-03-20 18:15:09 +0800

    更新README，整理代码

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Code to minimize
 
-> 用于本地压缩代码。
+> 用于快速的执行本地压缩代码，支持less，css，js代码多文件多输出压缩。
 
 ## 使用
 
@@ -10,10 +10,10 @@
 $ cnpm install
 ```
 
- - 需要压缩的js放入`app/js`目录中，执行：
+ - 需要压缩的js或css文件放入`app/(js|css)`目录中，执行：
 
 ```
 $ webpack
 ```
 
-> 生成压缩代码在`dist/js`路径中
+> 生成压缩代码在`dist/(js|css)`路径中
--- webpack.config.js
+++ webpack.config.js
@@ -3,85 +3,96 @@ const glob = require('glob');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
-// 动态查找所有js入口文件
-let entrys_js = {};
-glob.sync('./app/js/*.*').map(function (item) {
-    entrys_js[item.match(/[\w]+\./i)[0].slice(0, -1)] = item;
-});
-console.log('压缩js文件：', entrys_js);
+/**
+ * 动态查找多个入口(js|css)文件
+ */
+// 获取路径方法，默认js文件
+const getFileParams = (type = 'js') => {
+    let result = {};
+    glob.sync('./app/' + type + '/*.*').map(function (item) {
+        result[item.match(/[\w]+\./i)[0].slice(0, -1)] = item;
+    });
+    return result;
+};
+// js
+const entrysJS = getFileParams();
+console.log('压缩js文件：', entrysJS);
 
-// 动态查找所有css入口文件
-let entrys_css = {};
-glob.sync('./app/css/*.*').map(function (item) {
-    entrys_css[item.match(/[\w]+\./i)[0].slice(0, -1)] = item;
-});
-console.log('压缩css文件：', entrys_css);
+// css
+const entrysCSS = getFileParams('css');
+console.log('压缩css文件：', entrysCSS);
 
-module.exports = [{
+/**
+ * 公用plugins
+ */
+const pluginsCommon = [
+    new CleanWebpackPlugin(['dist/**/**'], {
+        root: __dirname
+    }),
+    // 设置node为生产环境
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+];
 
-    // mode
-    // mode: 'production',
+module.exports = [
+    /**
+     * js打包配置
+     */
+    {
+        // 入口文件
+        entry: entrysJS,
 
-    // js压缩，生产模式默认为开
-    // optimization: {
-    //     minimize: true
-    // },
+        // 输出文件
+        output: {
+            path: __dirname + '/dist',
+            filename: 'js/[name].min.js'
+        },
 
-    // 入口文件
-    entry: entrys_js,
-
-    // 输出文件
-    output: {
-        path: __dirname + '/dist',
-        filename: 'js/[name].min.js'
+        // plugins
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin()
+        ].concat(pluginsCommon)
     },
+    /**
+     * css打包配置
+     */
+    {
+        // 入口文件
+        entry: entrysCSS,
 
-    // plugins
-    plugins: [
-        new CleanWebpackPlugin(['dist/**/**'], {
-            root: __dirname
-        }),
-        new webpack.optimize.UglifyJsPlugin()
-    ]
-},{
+        // 输出文件
+        output: {
+            path: __dirname + '/dist',
+            filename: 'css/[name].min.css'
+        },
 
-    // 入口文件
-    entry: entrys_css,
-
-    // 输出文件
-    output: {
-        path: __dirname + '/dist',
-        filename: 'css/[name].min.css'
-    },
-
-    // module
-    module: {
-        rules: [
-            {
-                test: /\.(less|css)$/,
-                use: ExtractTextWebpackPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                minimize: true
+        // module
+        module: {
+            rules: [
+                {
+                    test: /\.(less|css)$/,
+                    use: ExtractTextWebpackPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    minimize: true
+                                }
+                            },
+                            {
+                                loader: 'less-loader'
                             }
-                        },
-                        {
-                            loader: 'less-loader'
-                        }
-                    ]
-                })
-            }
-        ]
-    },
+                        ]
+                    })
+                }
+            ]
+        },
 
-    // plugins
-    plugins: [
-        new CleanWebpackPlugin(['dist/**/**'], {
-            root: __dirname
-        }),
-        new ExtractTextWebpackPlugin('css/[name].min.css')
-    ]
-}];
+        // plugins
+        plugins: [
+            new ExtractTextWebpackPlugin('css/[name].min.css')
+        ].concat(pluginsCommon)
+    }
+];
