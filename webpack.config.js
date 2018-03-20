eb7b682d3ffabca7b4de69396f4a4a0fad6b585e
yujinpan <yujinpan@cmstop.com>
2018-03-20 16:00:01 +0800

    更换为webpack3，增加css的压缩

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,24 @@ const glob = require('glob');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
-// 动态查找所有入口文件
-let entrys = {};
+// 动态查找所有js入口文件
+let entrys_js = {};
 glob.sync('./app/js/*.*').map(function (item) {
-    entrys[item.match(/[\w]+\.js/i)[0].slice(0, -3)] = item;
+    entrys_js[item.match(/[\w]+\./i)[0].slice(0, -1)] = item;
 });
-console.log('压缩文件：', entrys);
+console.log('压缩js文件：', entrys_js);
 
-module.exports = {
+// 动态查找所有css入口文件
+let entrys_css = {};
+glob.sync('./app/css/*.*').map(function (item) {
+    entrys_css[item.match(/[\w]+\./i)[0].slice(0, -1)] = item;
+});
+console.log('压缩css文件：', entrys_css);
+
+module.exports = [{
 
     // mode
-    mode: 'production',
+    // mode: 'production',
 
     // js压缩，生产模式默认为开
     // optimization: {
@@ -21,12 +28,30 @@ module.exports = {
     // },
 
     // 入口文件
-    entry: entrys,
+    entry: entrys_js,
 
     // 输出文件
     output: {
         path: __dirname + '/dist',
         filename: 'js/[name].min.js'
+    },
+
+    // plugins
+    plugins: [
+        new CleanWebpackPlugin(['dist/**/**'], {
+            root: __dirname
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+},{
+
+    // 入口文件
+    entry: entrys_css,
+
+    // 输出文件
+    output: {
+        path: __dirname + '/dist',
+        filename: 'css/[name].min.css'
     },
 
     // module
@@ -57,6 +82,6 @@ module.exports = {
         new CleanWebpackPlugin(['dist/**/**'], {
             root: __dirname
         }),
-        // new ExtractTextWebpackPlugin('css/[name].min.css')
+        new ExtractTextWebpackPlugin('css/[name].min.css')
     ]
-};
+}];
